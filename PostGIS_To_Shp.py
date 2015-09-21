1e7849cc95bbe8b350adb37f0932244973302c95
Christy <christyheaton@gmail.com>
2015-09-21 16:48:54 -0700

    Update PostGIS_To_Shp.py

--- PostGIS_To_Shp.py
+++ PostGIS_To_Shp.py
@@ -1,5 +1,5 @@
 '''
-This script turns all of the tables in a list of postgis databases into tab files.
+This script turns all of the tables in a list of postgis databases into shapefiles.
 It must be run in the QGIS environment. Tested in QGIS 2.8.1.
 This script converts without considering size or breaking up files in to smaller chunks.
 '''
@@ -34,8 +34,8 @@ for database in databases:
         rows = vlayer.featureCount() 
         print database + ": " + table + " has " + str(rows) + " rows and " + str(numFields) + " columns."
 
-        # turn into TAB.
-        output = outputBase + database + "/" + table + ".tab"
+        # turn into shapefile.
+        output = outputBase + database + "/" + table + ".shp"
         QgsVectorFileWriter.writeAsVectorFormat(vlayer, output, encoding, coordsys, "ESRI Shapefile")
         print "Translation of " + database + ": " + table + ".tab successful."
 
